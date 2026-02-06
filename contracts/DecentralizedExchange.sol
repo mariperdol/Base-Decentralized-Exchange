bee5c82b1d1108e092631d0a7f06cfd6e2722144
Mari Perdol <AndrinaSellinger49@hotmail.com>
2026-02-06 06:34:13 -0600

    Update DecentralizedExchange.sol

--- contracts/DecentralizedExchange.sol
+++ contracts/DecentralizedExchange.sol
@@ -321,16 +321,7 @@ function getMarketStats() external view returns (
     // Implementation would return market statistics
     return (0, 0, 0, 0);
 }
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-
-contract DecentralizedExchange is Ownable, ReentrancyGuard {
-    using SafeMath for uint256;
 
 
     struct LimitOrder {
@@ -912,5 +903,5 @@ contract DecentralizedExchange is Ownable, ReentrancyGuard {
             avgPriceValue
         );
     }
-}
+
 }
