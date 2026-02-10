40c96a333a3a773d53a8b94dea61e449e0b00056
Mari Perdol <AndrinaSellinger49@hotmail.com>
2026-02-10 15:34:03 +0100

    Update DecentralizedExchange.sol

--- contracts/DecentralizedExchange.sol
+++ contracts/DecentralizedExchange.sol
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-/*
-  Educational router: 
-  - swapExactTokensForTokens
-  - minOut slippage protection
-*/
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
