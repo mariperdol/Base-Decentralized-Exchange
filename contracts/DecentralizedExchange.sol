0ddcb270b4cfe663a1d7ab03a11a9d2afee22480
Mari Perdol <AndrinaSellinger49@hotmail.com>
2026-02-11 15:00:51 +0100

    Update DecentralizedExchange.sol

--- contracts/DecentralizedExchange.sol
+++ contracts/DecentralizedExchange.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-  
 interface ILiquidityPool {
     function token0() external view returns (address);
     function token1() external view returns (address);
@@ -14,6 +15,8 @@ contract DecentralizedExchange {
     using SafeERC20 for IERC20;
 
     function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut) public pure returns (uint256) {
+        require(amountIn > 0, "in=0");
+        require(reserveIn > 0 && reserveOut > 0, "bad reserves");
         uint256 amountInWithFee = amountIn * 997;
         return (amountInWithFee * reserveOut) / (reserveIn * 1000 + amountInWithFee);
     }
@@ -35,7 +38,9 @@ contract DecentralizedExchange {
         address t1 = p.token1();
         require(tokenIn == t0 || tokenIn == t1, "bad tokenIn");
 
-        (uint256 reserveIn, uint256 reserveOut) = tokenIn == t0 ? (uint256(r0), uint256(r1)) : (uint256(r1), uint256(r0));
+        (uint256 reserveIn, uint256 reserveOut) =
+            tokenIn == t0 ? (uint256(r0), uint256(r1)) : (uint256(r1), uint256(r0));
+
         amountOut = getAmountOut(amountIn, reserveIn, reserveOut);
         require(amountOut >= minOut, "slippage");
 
@@ -45,3 +50,4 @@ contract DecentralizedExchange {
         else p.swap(amountOut, 0, to);
     }
 }
+
