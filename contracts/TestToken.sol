5716c0273fba818a780f4e2a0e60a622a8ad2cc6
Mari Perdol <AndrinaSellinger49@hotmail.com>
2026-02-24 10:51:57 +0100

    Update TestToken.sol

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
     constructor() ERC20("Test Token", "TEST") {
-        _mint(msg.sender, 1000000 * 10**18); // 1 million tokens
+        _mint(msg.sender, 1000000 * 10**18); 
     }
 
     function mint(address to, uint256 amount) external {
